e62bfc8ed8973acd8376b535dbbb0c6740fd004b
Nguyen Manh Dung <dungnm1@vnpay.vn>
2023-05-06 23:42:26 +0700

    first commit

---
+++ shared/_footer.php
@@ -0,0 +1,156 @@
+<section class="shop-method-area spad">
+    <div class="container">
+        
+        <div class="row d-flex justify-content-between">
+        <div class="header_logo">
+            <a href="index.html">Some Designer Info</a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="single-method mb-40">
+            <img src="./images/Calvin Richard Klein.jfif" alt="">
+            <h6>Calvin Klein</h6>
+            <p>Calvin Klein, full name Calvin Richard Klein, born in 1942, is a famous American fashion designer. He is the man who founded the Calvin Klein brand in 1968.</p>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+                                                        <div class="single-method mb-40">
+                                                            <img src="./images/Donatella Versace.jfif" alt="">
+                                                            <h6>Donatella Versace</h6>
+                                                            <p>Donatella Francesca Versace, informally known as Donatella Versace (born 1955), is an Italian fashion designer, businesswoman, socialite and model. She is the vice president of the board of directors and chief designer of the Versace Group.</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-3 col-md-6 col-sm-12">
+                                                        <div class="single-method mb-40">
+                                                            <img src="./images/Giorgio Armani.avif" alt="">
+                                                            <h6>Giorgio Armani</h6>
+                                                            <p>Giorgio Armani (born 1934) is an Italian fashion stylist. He is famous for designing men's clothing lines. Giorgio Armani used to study medicine, then gave up this profession to switch to photography, then became a fashion designer.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
+    <section class="instagram">
+                                            <div class="container-fluid">
+                                                <div class="row m-0">
+                                                    <div class="col-lg-2 p-0">
+                                                        <div class="instagram_item">
+                                                            <img src="./images/1.jpg">
+                                                            <div class="instagram_text">
+                                                                <i class="fa-brands fa-instagram"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-2 p-0">
+                                                        <div class="instagram_item">
+                                                            <img src="./images/2.jpg">
+                                                            <div class="instagram_text">
+                                                                <i class="fa-brands fa-instagram"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-2 p-0">
+                                                        <div class="instagram_item">
+                                                            <img src="./images/3.jpg">
+                                                            <div class="instagram_text">
+                                                                <i class="fa-brands fa-instagram"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-2 p-0">
+                                                        <div class="instagram_item">
+                                                            <img src="./images/1.jpg">
+                                                            <div class="instagram_text">
+                                                                <i class="fa-brands fa-instagram"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-2 p-0">
+                                                        <div class="instagram_item">
+                                                            <img src="./images/2.jpg">
+                                                            <div class="instagram_text">
+                                                                <i class="fa-brands fa-instagram"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-2 p-0">
+                                                        <div class="instagram_item">
+                                                            <img src="./images/3.jpg">
+                                                            <div class="instagram_text">
+                                                                <i class="fa-brands fa-instagram"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
+
+                                        <section class="footer spad">
+                                            <div class="container">
+                                                <div class="row">
+                                                    <div class="col-lg-4 col-md-6 col-sm-12">
+                                                        <div class="footer_about">
+                                                            <div class="footer_logo">
+                                                                <a href="index.html">Fashion Shop<span>.</span></a>
+                                                            </div>
+                                                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</p>
+                                                            <div class="footer_social">
+                                                                <a href="#">
+                                                                    <i class="fa-brands fa-facebook-f"></i>
+                                                                </a>
+                                                                <a href="#">
+                                                                    <i class="fa-brands fa-twitter"></i>
+                                                                </a>
+                                                                <a href="#">
+                                                                    <i class="fa-brands fa-instagram"></i>
+                                                                </a>
+                                                                <a href="#">
+                                                                    <i class="fa-brands fa-linkedin-in"></i>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-2 col-md-3">
+                                                        <div class="footer_widget">
+                                                            <h6>Quick Links</h6>
+                                                            <ul>
+                                                                <li><a href="#">About</a></li>
+                                                                <li><a href="#">Blogs</a></li>
+                                                                <li><a href="#">Services</a></li>
+                                                                <li><a href="#">FAQ</a></li>
+                                                                <li><a href="#">Contact</a></li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-2 col-md-3">
+                                                        <div class="footer_widget">
+                                                            <h6>Accounts</h6>
+                                                            <ul>
+                                                                <li><a href="#">My Account</a></li>
+                                                                <li><a href="#">Login</a></li>
+                                                                <li><a href="#">Order Tracking</a></li>
+                                                                <li><a href="#">Checkout</a></li>
+                                                                <li><a href="#">Wishlist</a></li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-4 col-md-3">
+                                                        <div class="footer_widget">
+                                                            <h6>Support</h6>
+                                                            <ul>
+                                                                <li><a href="#">Frequently Asked Questions</a></li>
+                                                                <li><a href="#">Terms & Conditions</a></li>
+                                                                <li><a href="#">Privacy Policy</a></li>
+                                                                <li><a href="#">Report a Payment Issue</a></li>
+                                                                <li><a href="#">24/7 Support</a></li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
+
+                                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
